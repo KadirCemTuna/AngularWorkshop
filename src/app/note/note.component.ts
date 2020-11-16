78b5889d82509c3f3ac849ced7efdbac042e25dc
kadircemtuna <kadir.tuna@etiya.com>
2020-11-16 14:57:43 +0300

    Note Component add note

--- src/app/note/note.component.ts
+++ src/app/note/note.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-note',
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./note.component.css']
 })
 export class NoteComponent implements OnInit {
+  title = '';
+  detail = '';
+  date = '';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  fncNoteSave(): void {
+    const oldData = localStorage.getItem('data');
+    const item = {title: this.title, detail: this.detail, date: this.date};
+    if (oldData == null){
+      // first user
+      const stObject = JSON.stringify([item]);
+      localStorage.setItem('data', stObject);
+    }else {
+      // second add
+      const oldArr = JSON.parse(oldData);
+      oldArr.push(item);
+      localStorage.setItem('data', JSON.stringify(oldArr));
+    }
+
   }
 
 }
